4a9f2d8964e4ec0090f96e635b7f18fb8990371e
laurirasanen <lauri@rasanen.dev>
2024-04-11 19:54:35 +0300

    fix camera, add debug cube

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,27 @@ mod terrain;
 
 use terrain::plugin::TerrainPlugin;
 
+#[derive(Component, Default)]
+struct CameraAngles {
+    pitch: f32,
+    yaw: f32,
+    roll: f32,
+}
+
 fn spawn_debug_cam(mut commands: Commands) {
-    let trans = Transform::from_xyz(0.0, 2.0, -2.0).looking_at(Vec3::ZERO, Vec3::Y);
-    commands.spawn(Camera3dBundle {
-        transform: trans,
-        ..Default::default()
-    });
+    let trans = Transform::from_xyz(0.0, 2.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y);
+    commands.spawn((
+        Camera3dBundle {
+            projection: PerspectiveProjection {
+                fov: 60.0_f32.to_radians(),
+                ..default()
+            }
+            .into(),
+            transform: trans,
+            ..Default::default()
+        },
+        CameraAngles { ..default() },
+    ));
     commands.spawn(PointLightBundle {
         transform: trans,
         ..Default::default()
@@ -21,33 +36,34 @@ fn spawn_debug_cam(mut commands: Commands) {
 fn move_camera(
     mut mouse: EventReader<MouseMotion>,
     kb_input: Res<ButtonInput<KeyCode>>,
-    mut cam_query: Query<&mut Transform, With<Camera>>,
+    mut cam_query: Query<&mut Transform, With<CameraAngles>>,
+    mut angles_query: Query<&mut CameraAngles>,
     time: Res<Time>,
 ) {
     let mut camera = cam_query.single_mut();
+    let mut angles = angles_query.single_mut();
+
+    let sensitivity = 4.0;
 
     for ev in mouse.read() {
         if f32::abs(ev.delta.x) > f32::EPSILON {
-            camera.rotate_local_z(ev.delta.x * 0.022);
+            angles.yaw -= ev.delta.x * 0.022 * sensitivity;
         }
         if f32::abs(ev.delta.y) > f32::EPSILON {
-            camera.rotate_local_x(ev.delta.y * 0.022);
-            let angle = camera.rotation.to_euler(EulerRot::XYZ).0;
-            let max = 0.5 * PI;
-            if angle < -max {
-                camera.rotate_local_x(-max - angle);
-            }
-            if angle > max {
-                camera.rotate_local_x(max - angle);
-            }
+            angles.pitch -= ev.delta.y * 0.022 * sensitivity;
+            angles.pitch = f32::clamp(angles.pitch, -89.0, 89.0);
         }
     }
+
+    camera.rotation = Quat::IDENTITY;
+    camera.rotate_y(angles.yaw.to_radians());
+    camera.rotate_local_x(angles.pitch.to_radians());
 
     let move_speed = 5.0;
     let delta = move_speed * time.delta_seconds();
 
     let fwd = camera.forward();
-    let right = camera.forward();
+    let right = camera.right();
     let up = Vec3::Y;
 
     if kb_input.pressed(KeyCode::KeyE) {
@@ -68,12 +84,6 @@ fn move_camera(
     if kb_input.pressed(KeyCode::ControlLeft) {
         camera.translation -= up * delta;
     }
-
-    info!(
-        "{:?} {:?} {:?}",
-        camera.translation.x, camera.translation.y, camera.translation.z,
-    );
-    info!("{:?}", camera.rotation.to_euler(EulerRot::XYZ),);
 }
 
 fn main() {
--- src/terrain/plugin.rs
+++ src/terrain/plugin.rs
@@ -16,15 +16,27 @@ impl TerrainPlugin {
     fn create_terrain(
         settings: Res<TerrainSettings>,
         mut commands: Commands,
-        mut mesh_assets: ResMut<Assets<Mesh>>,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<StandardMaterial>>,
     ) {
+        // test chunk
         let chunk = Chunk::new(&settings.fbm, 0, 0, 0);
-        let mesh_handle = mesh_assets.add(chunk.polygonize());
+        let mesh_handle = meshes.add(chunk.polygonize());
         let pbr = PbrBundle {
             mesh: mesh_handle,
+            material: materials.add(Color::GRAY),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..default()
         };
         commands.spawn((chunk, pbr));
+
+        // test cube
+        commands.spawn(PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(Color::RED),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        });
     }
 }
 
