ba5fe86600ed283fc919eb1c408d381ff59e1b8e
laurirasanen <lauri@rasanen.dev>
2024-04-12 16:33:27 +0300

    Add rapier plugin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use bevy::prelude::*;
-
 mod player;
 mod terrain;
 
+use bevy::prelude::*;
+use bevy_rapier3d::{
+    plugin::{NoUserData, RapierPhysicsPlugin},
+    render::RapierDebugRenderPlugin,
+};
 use player::plugin::PlayerPlugin;
 use terrain::plugin::TerrainPlugin;
 
@@ -10,6 +13,8 @@ fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(TerrainPlugin { seed: 1337 })
         .add_plugins(PlayerPlugin {})
         .run();
